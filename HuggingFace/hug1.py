bcd8581804b832374221c979ef496b706289c87f
Prabh <prabh2004p@gmail.com>
2025-09-13 01:35:06 -0400

    second commit

--- HuggingFace/hug1.py
+++ HuggingFace/hug1.py
@@ -1,27 +1,31 @@
 import streamlit as st
 from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from langchain_core.messages import HumanMessage
+from dotenv import load_dotenv
 
 # Get token: local env fallback or Streamlit Cloud secret
-import os
-HF_TOKEN = os.getenv("HUGGINGFACE_API_TOKEN") or st.secrets.get("HUGGINGFACE_API_TOKEN")
+load_dotenv()
 
 llm = HuggingFaceEndpoint(
     repo_id="google/gemma-2-9b-it",
-    task="conversational",
-    api_key=HF_TOKEN   # <- must be passed here
+    task="conversational" # <- must be passed here
 )
 
 model = ChatHuggingFace(llm=llm)
 
+
+if 'chat_history' not in st.session_state:
+    st.session_state.chat_history=[]
+ 
 # Streamlit UI
 st.header("Chat Bot Assistant")
-user_input = st.text_input("What do you want to ask?")
-topic_input = st.selectbox("Choose a topic", ["Technology", "Health", "Finance", "Education", "Entertainment"])
+user_input = st.text_input("You: ")
 style_input = st.selectbox("Choose a style", ["Formal", "Informal", "Humorous", "Serious", "Casual"])
 length_input = st.selectbox("Choose length", ["Short", "Medium", "Long"])
 
-if st.button("Answer"):
-    # Make sure you pass a string, not a Streamlit object
-    prompt = f"You are an expert in {topic_input}. Answer '{user_input}' in a {style_input} style and keep answer {length_input}."
-    response = model.invoke(prompt)
+if st.button("Answer") and user_input:
+    st.session_state.chat_history.append(HumanMessage(content=user_input))
+    prompt = f"You are helpful assistant. Answer '{user_input}' in a {style_input} style and keep answer {length_input}."
+    response = model.invoke(st.session_state.chat_history + [HumanMessage(content=prompt)])
+    st.session_state.chat_history.append(response)
     st.write(response.content)
